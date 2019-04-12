01aad9748005aec52c4edba00a2dfdb0425db629
Arup Sarkar <arup.sarkar@arupsarkar-ltm.internal.salesforce.com>
2019-04-12 16:01:50 -0400

    Added animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +19,8 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     AppRoutingModule,
     AppMaterial,
-    FlexLayoutModule
+    FlexLayoutModule,
+    BrowserAnimationsModule,
   ],
   exports: [
     FlexLayoutModule
