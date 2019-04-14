baf2fd02001950613f3640d85febd594965f72b7
Arup Sarkar <arup.sarkar@arupsarkar-ltm.internal.salesforce.com>
2019-04-14 10:39:49 -0400

    Added api layer

---
+++ api.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require("body-parser");
+
+router.use(bodyParser.urlencoded({
+  extended: true
+}));
+router.use(bodyParser.json());
+router.use(logErrors);
+router.use(clientErrorHandler);
+router.use(errorHandler);
+
+// Error handlers
+function logErrors (err, req, res, next) {
+  console.error(err.stack);
+  next(err)
+}
+function clientErrorHandler (err, req, res, next) {
+  if (req.xhr) {
+    res.status(500).send({ error: 'Something failed!' });
+  } else {
+    next(err)
+  }
+}
+
+function errorHandler (err, req, res, next) {
+  res.status(500);
+  res.render('error', { error: err })
+}
+
+/* GET api listing. */
+router.get('/', (req, res) => {
+  res.send('api works');
+});
+
+const db = require('./queries');
+
+// route middleware that will happen on every request
+router.use(function(req, res, next) {
+  // log each request to the console
+  console.log(req.method, req.url);
+  // continue doing what we were doing and go to the route
+  next();
+});
+
+router.get('/api', (req, res, next) => {
+  res.status(200).send({"data " : "hello world !!!"});
+});
+
+router.get('/leads', db.getLeads);
+router.get('/leads/:id', db.getLeadById);
+router.post('/leads', db.createLead);
+router.put('/leads/:id', db.updateLead);
+router.delete('/leads/:id', db.deleteLead);
+
+module.exports = router;
+
--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const session = require("express-session");
 const bodyParser = require("body-parser");
 const cors = require('cors');
 const path =require("path");
+const app = express();
+// Setup HTTP Server
+const server = require('http').Server(app);
 const port = process.env.PORT || 3003;
 
-const db = require('./queries');
-// Setup HTTP Server
-const app = express();
-const router = express.Router();
+
+
+// const router = express.Router();
 // Initialize session
 app.use(session(
   {
@@ -18,7 +20,10 @@ app.use(session(
   }
 ));
 app.use(cors());
-// app.options('*', cors());
+// Get our API routes
+const api = require('./api');
+// Set our api routes
+app.use('/api', api);
 // bodyParser
 app.use(bodyParser.json());
 app.use(
@@ -28,28 +33,5 @@ app.use(express.static(__dirname + '/dist/rad-demo-tool'));
 app.use('/*', function(req,res) {
   res.sendFile(path.join(__dirname+'/dist/rad-demo-tool/index.html'));
 });
-//router call -START
 
-// route middleware that will happen on every request
-router.use(function(req, res, next) {
-  // log each request to the console
-  console.log(req.method, req.url);
-  // continue doing what we were doing and go to the route
-  next();
-});
-
-router.get('/api', (req, res, next) => {
-  res.status(200).send({"data " : "hello world !!!"});
-});
-
-router.get('/leads', db.getLeads);
-router.get('/leads/:id', db.getLeadById);
-router.post('/leads', db.createLead);
-router.put('/leads/:id', db.updateLead);
-router.delete('/leads/:id', db.deleteLead);
-
-app.use('/', router);
-//router call -END
-app.listen(port, function (req, res) {
-  console.log('RAD Demo tool node server running on port ' + port);
-});
+server.listen(port, () => console.log(`API server running on localhost:${port}`));
