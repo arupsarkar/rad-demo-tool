d08e1228897dc7b7b1f58c28d3d5cb0b8f241923
Arup Sarkar <arup.sarkar@arupsarkar-ltm.internal.salesforce.com>
2019-04-13 21:56:40 -0400

    Updated app.use instead of app.get in serving index.html

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(
   bodyParser.urlencoded({extended: true})
 );
 app.use(express.static(__dirname + '/dist/rad-demo-tool'));
-app.get('/*', function(req,res) {
+app.use('/*', function(req,res) {
   res.sendFile(path.join(__dirname+'/dist/rad-demo-tool/index.html'));
 });
 //router call -START
