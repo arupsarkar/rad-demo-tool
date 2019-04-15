690f9fd606ab6c1ce3cc78ac625d7899d9a44bef
Arup Sarkar <arup.sarkar@arupsarkar-ltm.internal.salesforce.com>
2019-04-15 01:00:27 -0400

    Added correct bind params

--- queries.js
+++ queries.js
@@ -53,14 +53,14 @@ const createLead = (request, response) => {
 
 const updateLead = (request, response) => {
   const leadId = parseInt(request.params.id);
-  const { email, firstname, lastname,mobilephone,postalcode, sms_opt_in__c, id  } = request.body;
+  const { email, firstname, lastname,mobilephone,postalcode, sms_opt_in__c  } = request.body;
   console.log('---> updateLead : ', request.body);
   console.log('---> updateLead Id : ', leadId);
   pool.query(
     'UPDATE leads SET email = $1, firstname = $2, lastname = $3, mobilephone = $4, postalcode = $5, sms_opt_in__c = $6 ' +
       'WHERE id = ' + leadId + ' ' +
       'RETURNING id, firstname, lastname, email, mobilephone, postalcode, sms_opt_in__c',
-    [email, firstname, lastname,mobilephone,postalcode, sms_opt_in__c, id],
+    [email, firstname, lastname,mobilephone,postalcode, sms_opt_in__c],
     (error, results) => {
       if (error) {
         throw error
